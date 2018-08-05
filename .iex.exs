979106321a8d6e13e1ab8d0d3f7b12175c32473b
Julián R Figueroa <julian@liztum.com>
2018-08-05 04:23:35 -0500

    Aliases file

---
+++ .iex.exs
@@ -0,0 +1,8 @@
+# Aliases preloaded for the iex console
+alias QuieroSaber.Repo
+alias QuieroSaber.Poll
+alias QuieroSaber.Session
+alias QuieroSaber.Question
+alias QuieroSaber.Option
+alias QuieroSaber.Participant
+alias QuieroSaber.Answer
