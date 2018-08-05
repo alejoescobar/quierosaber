6cc82b8b896764d66691270f657d0fd436e8f8f6
Julián R Figueroa <julian@liztum.com>
2018-08-05 00:56:03 -0500

    new subdomain

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :quiero_saber, QuieroSaber.Endpoint,
   load_from_system_env: true,
   http: [port: System.get_env("PORT")],
-  url: [scheme: "https", host: "quierosaber.co", port: 443],
+  url: [scheme: "https", host: "angelhack.quierosaber.co", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
