77826be0d5624b24db662d755095d628c8c9bcf5
Julián R Figueroa <julian@liztum.com>
2018-08-04 23:59:09 -0500

    Name of Heroku app

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :quiero_saber, QuieroSaber.Endpoint,
   load_from_system_env: true,
   http: [port: System.get_env("PORT")],
-  url: [scheme: "https", host: "desolate-reef-39722.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "peaceful-journey-75904.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
