a1067f9682d4b25abff4fb022d5331f6807890c3
Julián R Figueroa <julian@liztum.com>
2018-08-05 02:41:08 -0500

    POST participants

--- web/controllers/api_participant_controller.ex
+++ web/controllers/api_participant_controller.ex
@@ -1,11 +1,24 @@
+require Logger
+
 defmodule QuieroSaber.ApiParticipantController do
   use QuieroSaber.Web, :controller
 
-  alias QuieroSaber.ApiParticipant
+  alias QuieroSaber.Participant
+  alias QuieroSaber.Session
+
+  def action(conn, _) do
+    session = Repo.get!(Session, conn.params["api_session_id"])
+    Logger.debug "Session: #{inspect(session)}"
+    args = [conn, conn.params, session]
+    apply(__MODULE__, action_name(conn), args)
+  end
 
   def create(conn, %{"participant" => participant}, session) do
-    Logger.debug "Session: #{inspect(session)}"
-    Logger.debug "Participant: #{inspect(participant)}"
-    render conn, "show.json", participant: %Participant{}
+    participant = Repo.insert!(%Participant{
+     device_id: participant["device_id"],
+     nickname: participant["nickname"],
+     session_id: session.id
+    })
+    render conn, "show.json", participant: participant
   end
 end
--- web/views/api_participant_view.ex
+++ web/views/api_participant_view.ex
@@ -3,17 +3,22 @@ defmodule QuieroSaber.ApiParticipantView do
 
   def render("show.json", %{participant: participant}) do
     %{
-      participant: participant
+      participant: participant_json(participant)
     }
   end
 
-
+  def render("message.json", %{message: message}) do
+    %{
+      message: message
+    }
+  end
 
   def participant_json(participant) do
     %{
+      id: participant.id,
       device_id: participant.device_id,
       nickname: participant.nickname,
-      session: participant.session,
+      session_id: participant.session_id,
     }
   end
 end
