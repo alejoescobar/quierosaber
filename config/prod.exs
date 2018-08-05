61fd159a7b91aecf4527f48a541b123a19c5e9fc
Julián R Figueroa <julian@liztum.com>
2018-08-05 02:05:07 -0500

    Changes SSL

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,8 @@ config :quiero_saber, QuieroSaber.Endpoint,
   url: [scheme: "https", host: "angelhack.quierosaber.co", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  check_origin: false
 
 # Do not print debug messages in production
 config :logger, level: :info
